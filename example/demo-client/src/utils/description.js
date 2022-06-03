1554853cec66af4dc783d912e7d0f54d0a55d4ee
Nikos Fotos <nickfotosgr@gmail.com>
2022-06-03 16:43:31 +0200

    Support provenance boxes in example app

--- example/demo-client/src/utils/description.js
+++ example/demo-client/src/utils/description.js
@@ -7,7 +7,14 @@ export const contentTypeUuidToText = {
     '786D6C20-0011-0010-8000-00AA00389B71': 'XML Content type JUMBF box',
     '40CB0C32-BB8A-489D-A70B-2AD6F47F4369': 'Embedded File Content type JUMBF box',
     'DC28B95F-B68A-498E-8064-0FCA845D6B0E': 'Replacement Content type JUMBF box',
-    '74B11BBF-F21D-4EEA-98C1-0BEBF23AEFD3': 'Protection Content type JUMBF box'
+    '74B11BBF-F21D-4EEA-98C1-0BEBF23AEFD3': 'Protection Content type JUMBF box',
+    '6D707374-C65D-11EC-9D64-0242AC120002': 'Manifest Store Content type JUMBF box',
+    '6D70736D-C65D-11EC-9D64-0242AC120002': 'Standard Manifest Content type JUMBF box',
+    '6D70756D-C65D-11EC-9D64-0242AC120002': 'Update Manifest Content type JUMBF box',
+    '6D70636C-0011-0010-8000-00AA00389B71': 'Claim Content type JUMBF box',
+    '6D706373-0011-0010-8000-00AA00389B71': 'Claim Signature Content type JUMBF box',
+    '6D706173-C65D-11EC-9D64-0242AC120002': 'Assertion Store Content type JUMBF box',
+    '6D707663-0011-0010-8000-00AA00389B71': 'Credentials Store Content type JUMBF box'
 }
 
 export const bmffBoxTypeToText = {
